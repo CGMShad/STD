7d2ccef5fd11a2e8f59271be963eae27bb4b1b88
Chrst <Clement.chrst@eduge.ch>
2020-08-31 11:37:47 +0200

    Exercice 01 v1.0

---
+++ Exercice01/CC_InvertColorBmp/CC_InvertColorBmp/FrmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CC_InvertColorBmp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opfImage = new System.Windows.Forms.OpenFileDialog();
+            this.btnPickImage = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.sfdNewImage = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // opfImage
+            // 
+            this.opfImage.FileName = "openFileDialog1";
+            // 
+            // btnPickImage
+            // 
+            this.btnPickImage.Location = new System.Drawing.Point(354, 12);
+            this.btnPickImage.Name = "btnPickImage";
+            this.btnPickImage.Size = new System.Drawing.Size(167, 44);
+            this.btnPickImage.TabIndex = 0;
+            this.btnPickImage.Text = "Pick image";
+            this.btnPickImage.UseVisualStyleBackColor = true;
+            this.btnPickImage.Click += new System.EventHandler(this.btnPickImage_Click);
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Location = new System.Drawing.Point(354, 74);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(167, 44);
+            this.btnInvert.TabIndex = 1;
+            this.btnInvert.Text = "Invert";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(12, 12);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(284, 270);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 3;
+            this.pbImage.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(354, 136);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(165, 44);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 301);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnPickImage);
+            this.Name = "FrmMain";
+            this.Text = "Invert Color bmp";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog opfImage;
+        private System.Windows.Forms.Button btnPickImage;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog sfdNewImage;
+    }
+}
+
---
+++ Exercice01/CC_InvertColorBmp/CC_InvertColorBmp/FrmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CC_InvertColorBmp
+{
+    public partial class FrmMain : Form
+    {
+        Bitmap logo, invertLogo;
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            opfImage.Title = "Open Bitmap";
+            opfImage.Filter = "bmp files (*.bmp)|*.bmp";
+            sfdNewImage.Filter = "bmp files (*.bmp)|*.bmp";
+            sfdNewImage.FileName = "resultImage";
+            btnInvert.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(sfdNewImage.ShowDialog() == DialogResult.OK)
+            {
+                pbImage.Image.Save(sfdNewImage.FileName, ImageFormat.Bmp);
+                logo = null;
+                btnInvert.Enabled = false;
+                pbImage.Image = null;
+            }
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            pbImage.Image = null;
+            invertLogo = logo;
+            for (int x = 0; x < 1024; x++)
+            {
+                for (int y = 0; y < 1024; y++)
+                {
+                    invertLogo.SetPixel(x, y, Color.FromArgb(255 - invertLogo.GetPixel(x, y).ToArgb()));
+                }
+            }
+            pbImage.Image = invertLogo;
+        }
+
+        private void btnPickImage_Click(object sender, EventArgs e)
+        {
+            if(opfImage.ShowDialog() == DialogResult.OK)
+            {
+                logo = new Bitmap(opfImage.FileName);
+                if(logo != null)
+                {
+                    btnInvert.Enabled = true;
+                    pbImage.Image = logo;
+                }
+            }
+            
+        }
+    }
+}
